cde48c285e1dcacca2cc5546d65f90307cb50fac
NiRMIT-KuMAR <nirmitkumar2006@gmail.com>
2025-10-30 12:44:07 +0530

    Practice with variables and data types

---
+++ day_2.py
@@ -0,0 +1,15 @@
+# Personal Information
+
+student_name = "Nirmit Vaja"
+city = "Ahmedabad"
+current_year = 2025
+age = 19
+gpa = 9.8
+is_enrolled = True
+
+# Printing Personal Information
+
+print(f"Student Name : {student_name} ")
+print(f"Current Location : {city}")
+print(f"GPA in {current_year} : {gpa}")
+print(f"Is currently Enrolled : {is_enrolled}")
