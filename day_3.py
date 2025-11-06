5493795cb2467a79bbdf3f4ae66ea673d0d1cd47
NiRMIT-KuMAR <nirmitkumar2006@gmail.com>
2025-11-06 12:28:23 +0530

    Practiceing with user input and type coversion.

---
+++ day_3.py
@@ -0,0 +1,13 @@
+# Getting  user info.
+
+user_name = input("What is your name?")
+user_age = int(input("What is your age?"))
+user_gpa = float(input("What is your gpa?"))
+
+# Summary
+print(f"User Name : {user_name}")
+print(f"Age : {user_age}")
+print(f"GPA : {user_gpa}")
+
+# Calculating age 
+print(f"{user_name} will be {user_age + 5} years old  after 5 years.")
