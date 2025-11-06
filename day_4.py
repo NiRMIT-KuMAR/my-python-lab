13c4290d9062686bed432efb4173a4d0443ca1eb
NiRMIT-KuMAR <nirmitkumar2006@gmail.com>
2025-11-06 12:48:25 +0530

    Practice with conditional logic (if/elif/else)

---
+++ day_4.py
@@ -0,0 +1,12 @@
+# Age Checker
+
+age = int(input("How old are you ?"))
+
+if age < 13 :
+    print("You are a child")
+elif age >= 13 and age <= 19 : 
+    print("You are a teanager")  
+elif age >= 65 :
+    print("You are a senier citizen")      
+else :
+    print("You are an adult")    
