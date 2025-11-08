a0d4473f48ea7a18167de648b1742d2687eefc7e
NiRMIT-KuMAR <nirmitkumar2006@gmail.com>
2025-11-08 10:02:25 +0530

    Practice with defining and calling functions

---
+++ day_8.py
@@ -0,0 +1,12 @@
+def show_menu():
+  print("1. Add Task : ")
+  print("2. View task : ")
+  print("3. Exit :")
+
+def add_numbers(num1,num2):
+  print(num1+num2)
+
+
+show_menu()
+add_numbers(100,-5) 
+add_numbers(10,5)
