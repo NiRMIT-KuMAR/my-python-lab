d8831f51a04deac45cb76517c5bbd0e9f5429348
NiRMIT-KuMAR <nirmitkumar2006@gmail.com>
2025-11-09 11:56:32 +0530

    Practice with lists of dictionaries

---
+++ day_12.py
@@ -0,0 +1,7 @@
+books = [
+  {"Title" : "The Great Gatsby" , "Author" : "F.Scott Fitzgerald"},
+  {"Title" : "The Pragmatic Programmer" , "Author" : "David Thomas"}
+]
+
+for book in books:
+  print(f"{book["Title"]} by {book["Author"]}")
